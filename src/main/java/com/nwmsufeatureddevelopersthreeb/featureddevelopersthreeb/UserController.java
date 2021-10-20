c460a8d848c77d9cf5b8b76b39f027c0232fda4c
KeerthiMuli <77706824+KeerthiMuli@users.noreply.github.com>
2021-10-19 21:51:33 -0500

    Controller Class for user is created

---
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/UserController.java
@@ -0,0 +1,13 @@
+package com.nwmsufeatureddevelopersthreeb.featureddevelopersthreeb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.List;
+@RestController
+public class UserController {
+	 @Autowired
+	    UserRepository userRepository;
+	 // User Registration
+	
+
+}
