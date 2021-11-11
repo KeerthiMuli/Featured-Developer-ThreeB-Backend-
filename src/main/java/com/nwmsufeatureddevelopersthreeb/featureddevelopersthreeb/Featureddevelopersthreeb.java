2314062dc7f600b179260cac1ff2b746b0ce6b61
KeerthiMuli <77706824+KeerthiMuli@users.noreply.github.com>
2021-11-10 18:27:37 -0600

    annotations are added to the main class

--- src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/Featureddevelopersthreeb.java
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/Featureddevelopersthreeb.java
@@ -1,20 +1,16 @@
 package com.nwmsufeatureddevelopersthreeb.featureddevelopersthreeb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class Featureddevelopersthreeb{
-
 	public static void main(String[] args) {
 SpringApplication.run(Featureddevelopersthreeb.class, args);
-	
-        
+    
         }
 
 }
