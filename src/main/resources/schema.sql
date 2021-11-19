49462c03ec4dc1b6a630c4d97df800a164aa39d8
KeerthiMuli <77706824+KeerthiMuli@users.noreply.github.com>
2021-11-19 09:18:50 -0600

    reverting the changes of adding scripts to tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,31 +7,3 @@
  * Author:  S541906
  * Created: Nov 17, 2021
  */
-
-CREATE TABLE public.developers(
-DeveloperId varchar(100) Primary Key,
-FullName varchar(100),
-EmailAddress varchar(100),
-Goals varchar(1000),
-Skills varchar(100),
-LinkedInURL varchar(150),
-GitHubURL varchar(150),
-Image text
-);
-
-
- CREATE TABLE public.users (
-    id integer NOT NULL,
-    username character varying(50),
-    password text,
-    loggedin boolean,
-    confirmpassword text
-);
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
