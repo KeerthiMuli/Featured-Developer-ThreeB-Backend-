b09bc1fdb7ada886c73d27fd9b3abc22cf5ecad3
KeerthiMuli <77706824+KeerthiMuli@users.noreply.github.com>
2021-11-19 18:01:23 -0600

    columns datatypes are modified

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,18 +4,18 @@
  * Created: Nov 17, 2021
  */
 CREATE TABLE public.developers(
-DeveloperId varchar(100) Primary Key,
-FullName varchar(100),
-EmailAddress varchar(100),
-Goals varchar(1000),
-Skills varchar(100),
-LinkedInURL varchar(150),
-GitHubURL varchar(150),
+DeveloperId text Primary Key,
+FullName text,
+EmailAddress text,
+Goals text,
+Skills text,
+LinkedInURL text,
+GitHubURL text,
 Image text
 );
  CREATE TABLE public.users (
     id integer NOT NULL,
-    username character varying(50),
+    username text,
     password text,
     loggedin boolean,
     confirmpassword text
