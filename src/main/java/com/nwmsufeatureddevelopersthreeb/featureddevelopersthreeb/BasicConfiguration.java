0ac2d00cbae2ce0c85b826342f1db102dd5d0b67
KeerthiMuli <77706824+KeerthiMuli@users.noreply.github.com>
2021-11-29 13:39:45 -0600

    modified web security configuration setup

--- src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/BasicConfiguration.java
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/BasicConfiguration.java
@@ -12,6 +12,9 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/").permitAll()
-                            
+                                .antMatchers(HttpMethod.GET, "/developers/random").permitAll()
+				.antMatchers(HttpMethod.GET, "/developers/randomshow").permitAll()
+				.antMatchers(HttpMethod.GET, "/developers/getall")  
+				.permitAll().anyRequest().authenticated();
 	}
 }
