e332f75893ca58a6d01729925622c0ce1dd30c8f
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-02-03 15:23:33 -0600

    updated user images links in developers data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -41,21 +41,21 @@ INSERT INTO public.developers(
 	VALUES 
 ('S541994','Narsing Rao Nikitha Madhari','nikithamn30@gmail.com','To become a data scientist',
 'Java, SQL, Python, HTML','https://www.linkedin.com/in/nikitha-mn-42b2451aa/',
-'https://github.com/NikithaMN-05','https://media-exp1.licdn.com/dms/image/C5603AQGFpMBjvgubmg/profile-displayphoto-shrink_400_400/0/1595495277739?e=1641427200&v=beta&t=8gbdvajFWLvyAJXd19282WsbmAp-38U3FAdGIAbvNMM');
+'https://github.com/NikithaMN-05','https://logos.textgiraffe.com/logos/logo-name/28271959-designstyle-autumn-m.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
 	VALUES 
 ('S542406','Sai Manideep Allu','manideepml@gmail.com','To become a successful product manager.',
 'Java, Python & ML tech stack','https://www.linkedin.com/in/sai-manideep-allu/',
-'https://github.com/saimanideepallu','https://media-exp1.licdn.com/dms/image/C5603AQG-lrBhIqknww/profile-displayphoto-shrink_400_400/0/1631167688058?e=1641427200&v=beta&t=AES4Ai-dwwj1lvhtmHT6QubrgAA0UUAEMnj_6eyZYmU');
+'https://github.com/saimanideepallu','https://logos.textgiraffe.com/logos/logo-name/28271978-designstyle-autumn-m.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
 	VALUES 
 ('S541999','Abhilash Ramavaram','abhilashramavaram@gmail.com','Aspire to become bigdata architect',
 'Java , Scala,spark,python, Hdfs, aws','https://www.linkedin.com/in/abhilash-ramavaram-30a23412b',
-'https://github.com/AbhiRam0099','https://media-exp1.licdn.com/dms/image/C5603AQEOzVpSzeum9w/profile-displayphoto-shrink_400_400/0/1597677122465?e=1641427200&v=beta&t=oOmnMQN5TsBs3o_b9gWctCgp5PdJpX_ZD45DRI6C7i8');
+'https://github.com/AbhiRam0099','https://logos.textgiraffe.com/logos/logo-name/Abhilash-designstyle-autumn-l.png');
 
 
 INSERT INTO public.developers(
@@ -63,7 +63,7 @@ INSERT INTO public.developers(
 	VALUES 
 ('S542392','Jaya Kumar Saga','Jayakumar1994@gmail.com','To become Architect',
 'Java,Vue, WebMethods, SQL','https://www.linkedin.com/in/jaya-kumar-saga/',
-'https://github.com/sagajayakumar/sagajayakumar','https://media-exp1.licdn.com/dms/image/C4E03AQGjXRymUrNNFA/profile-displayphoto-shrink_400_400/0/1517256401406?e=1641427200&v=beta&t=7vEI0RLJYlF1l_5FPbVII9UxcL4xzZP28hDJix9iMSI');
+'https://github.com/sagajayakumar/sagajayakumar','https://logos.textgiraffe.com/logos/logo-name/28272066-designstyle-autumn-l.png');
 
 
 INSERT INTO public.developers(
@@ -71,7 +71,7 @@ INSERT INTO public.developers(
 	VALUES 
 ('S541906','Keerthi Muli','keerthi.11rdy@gmail.com','Aspiring to become an Java Backend Developer',
 'Java, HTML, CSS, SQL','https://www.linkedin.com/in/mkeerthi/',
-'https://github.com/KeerthiMuli','https://media-exp1.licdn.com/dms/image/C5603AQHsyYCGe_ngoA/profile-displayphoto-shrink_200_200/0/1636139237261?e=1642636800&v=beta&t=LubQyosGSBEz5qQA09IUpYpVFsuINBEKbKnC4EVRWOI');
+'https://github.com/KeerthiMuli','https://logos.textgiraffe.com/logos/logo-name/28272108-designstyle-autumn-l.png');
 
 
 INSERT INTO public.developers(
@@ -94,28 +94,28 @@ INSERT INTO public.developers(
 	VALUES 
 ('S542000','Naveen Tanuku','ntanuku96@gmail.com','To be an entrepreneur',
 'Java, Power BI, ETL tools, Web technologies','https://www.linkedin.com/in/naveen-tanuku/',
-'https://github.com/NaveenTanuku','https://media-exp1.licdn.com/dms/image/C5603AQH4DVLFvdRFew/profile-displayphoto-shrink_400_400/0/1611015575996?e=1641427200&v=beta&t=N1ERjeM5djBKTwRniyx2t-jYnWaBGDcvo79T9wLf8XA');
+'https://github.com/NaveenTanuku','https://logos.textgiraffe.com/logos/logo-name/Naveen-designstyle-autumn-l.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
 	VALUES 
 ('S542298','Sri Sathya Mamidala','sathya.mamidala1@gmail.com','To become a backend java developer',
 'Java, HTML','https://www.linkedin.com/in/sathya-mamidala-363b6a201/',
-'https://github.com/srisathyamamidala','https://media-exp1.licdn.com/dms/image/C5603AQFrWpYYvoNhUA/profile-displayphoto-shrink_400_400/0/1607868548096?e=1641427200&v=beta&t=0dAIVKNxpOqyH_puxXsx-awfZefoxZs5fPsaJBTlfY4');
+'https://github.com/srisathyamamidala','https://logos.textgiraffe.com/logos/logo-name/28272131-designstyle-autumn-l.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
 	VALUES 
 ('S542269','Shivaram Kothapally','shivaramkothapally177@gmail.com','To became a leader in the software market',
 'Html, java, Nodejs',':www.linkedin.com/in/shivaram-kothapally-a813aa179',
-'https://github.com/shivaramkothapally','https://media-exp1.licdn.com/dms/image/C5603AQFDPiwt0X8HzQ/profile-displayphoto-shrink_400_400/0/1612484057453?e=1641427200&v=beta&t=uJWGZN82NOZjoKGy0dkDQRMC60hjcZA5c9KLKL_ln6M');
+'https://github.com/shivaramkothapally','https://logos.textgiraffe.com/logos/logo-name/28272140-designstyle-autumn-l.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
 	VALUES 
 ('S542271','Tejaswi Reddy Nallavolu','Tejaswireddynallavolu@gmail.com','To become a Front-end developer',
 'HTML,CSS, JavaScript, Bootstrap, Java','https://www.linkedin.com/in/tejaswi-reddy-3a69a417b/',
-'https://github.com/tejaswinallavolu','https://media-exp1.licdn.com/dms/image/C4E03AQHcsgrpVSxBfg/profile-displayphoto-shrink_400_400/0/1633751606973?e=1641427200&v=beta&t=QSePPcEuCkLbLzTDgM6k-Ax1ft8W355mIk_Ipq8YR0g');
+'https://github.com/tejaswinallavolu','https://logos.textgiraffe.com/logos/logo-name/28272145-designstyle-autumn-l.png');
 
 
 INSERT INTO public.developers(
@@ -123,35 +123,35 @@ INSERT INTO public.developers(
 	VALUES 
 ('S542048','Vivek Tadiparthi','tadsaivivekreddy@gmail.com','Aspire to be an App developer and have a startup of my own.',
 'Java, Python, Android development','https://www.linkedin.com/in/vivektadiparthi/',
-'https://github.com/vivektadiparthi','https://media-exp1.licdn.com/dms/image/C4D03AQEmUX5ba_R4gA/profile-displayphoto-shrink_400_400/0/1610239019881?e=1641427200&v=beta&t=oIrdT92EvcW3hHyPfxgp1MxBgOdOsuahtckl5m3GZOc');
+'https://github.com/vivektadiparthi','https://logos.textgiraffe.com/logos/logo-name/Vivek-designstyle-autumn-l.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
 	VALUES 
 ('S541903','Pramod Reddy Gonegari','gonegaripramodreddy@gmail.com','To build a product based startup and become an entrepreneur.',
 'Mobile and Web Application Development, Vue, Ionic, Java, JavaScript .','https://www.linkedin.com/in/pramod-reddy-gonegari/',
-'https://github.com/pramod096','https://media-exp1.licdn.com/dms/image/C5603AQG_WTWVfbSuDA/profile-displayphoto-shrink_400_400/0/1602683531447?e=1641427200&v=beta&t=HJZnN1dFlC98XkJkSMWMcJhOWBDl80Di-l2-jCy6nw0');
+'https://github.com/pramod096','https://logos.textgiraffe.com/logos/logo-name/28272161-designstyle-autumn-l.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
 	VALUES 
 ('S542264','Rasagna Reddy Banda','rasagna.reddyc@gmail.com','To work as a Frontend developer',
 'Java, HTML, CSS, Java script','https://www.linkedin.com/in/rasagna-reddy-86a39a170/',
-'https://github.com/Rasagna0409','https://media-exp1.licdn.com/dms/image/C5603AQG-GE4WVT27bw/profile-displayphoto-shrink_400_400/0/1624504209186?e=1641427200&v=beta&t=1TQfk1xETmJ6knM_8UdyEb4ZTT-nKK1PeQTFWtCqBqI');
+'https://github.com/Rasagna0409','https://logos.textgiraffe.com/logos/logo-name/28272169-designstyle-autumn-l.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
 	VALUES 
 ('S542295','Abhilash Dhumala','dhumala.abhilash@gmail.com','To be a Front-end developer.',
 'Java, HTML, UFT.',' https://www.linkedin.com/in/abhilash-dhumala-4b4ab9172/',
-'https://github.com/Abhilash15','https://logos.textgiraffe.com/logos/logo-name/Abhilash-designstyle-summer-m.png');
+'https://github.com/Abhilash15','https://logos.textgiraffe.com/logos/logo-name/28272177-designstyle-birthday-l.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
 	VALUES 
 ('S542046','Akanksha Sudhagoni','akankshagoud.s@gmail.com','To become a Full-stack developer',
 'Html, Java, CSS.','https://www.linkedin.com/in/akanksha-goud-aa0ab8211',
-'https://github.com/S542046','https://media-exp1.licdn.com/dms/image/C4E03AQH95DFFa0Mhpg/profile-displayphoto-shrink_400_400/0/1622045493003?e=1641427200&v=beta&t=dtUUvj0ht76URCUdNUC-9f_TpaPT0jWdzcq-aFWeL8Q');
+'https://github.com/S542046','https://logos.textgiraffe.com/logos/logo-name/28272187-designstyle-autumn-l.png');
 
 
 INSERT INTO public.developers(
@@ -159,7 +159,7 @@ INSERT INTO public.developers(
 	VALUES 
 ('S542308','Arnav Akula','arnavakula3@gmail.com','Data engineer',
 'Python , java, web techonolgies ','https://www.linkedin.com/in/arnav-akula-76a649200/',
-'https://github.com/Arnavakula7474','https://media-exp1.licdn.com/dms/image/C4E03AQEbJskva37EAg/profile-displayphoto-shrink_400_400/0/1620751830859?e=1641427200&v=beta&t=1YMRkUsi14e0KnPDB-Hzmh_pDrB0QYBYFECsAtTBuKU');
+'https://github.com/Arnavakula7474','https://logos.textgiraffe.com/logos/logo-name/Arnav-designstyle-autumn-l.png');
 
 
 INSERT INTO public.developers(
@@ -167,7 +167,7 @@ INSERT INTO public.developers(
 	VALUES 
 ('S542409','Harshavardhan Kurra','harshavardhankurra4@gmail.com','Software Developer',
 'Python, Java, Html, JavaScript','https://www.linkedin.com/in/harsha-kurra-b25b3b177',
-'https://github.com/harshakurra123','https://media-exp1.licdn.com/dms/image/C5103AQHO2jMh_vVajA/profile-displayphoto-shrink_400_400/0/1545151738190?e=1641427200&v=beta&t=lWB9qGy1nbYIlhORUrH4P6O_SOxtfYn2oCiExvaw3ns');
+'https://github.com/harshakurra123','https://logos.textgiraffe.com/logos/logo-name/Harsha-designstyle-autumn-l.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
@@ -175,7 +175,7 @@ INSERT INTO public.developers(
 ('S541669','Sanjay Saripalli','sanjaysaripalli96@gmail.com','To be an Machine Learning Engineer',
 'Languages : Python, Java, SQL
 Libraries & Frameworks : NumPy, Pandas, Matplotlib, Keras, Scikit-learn, Seaborn','https://www.linkedin.com/in/sanjay-saripalli-57242b203/',
-'https://github.com/sanjay-13','https://media-exp1.licdn.com/dms/image/C5603AQEYSZQDPDbktg/profile-displayphoto-shrink_400_400/0/1612546441551?e=1641427200&v=beta&t=8N2osG3fPA6xQJ5ocwb2h98Wb5HHuAuBGtroAHQ75Lc');
+'https://github.com/sanjay-13','https://logos.textgiraffe.com/logos/logo-name/Sanjay-designstyle-autumn-l.png');
 
 INSERT INTO public.developers(
 	developerid, fullname, emailaddress, goals, skills, linkedinurl, githuburl,image)
