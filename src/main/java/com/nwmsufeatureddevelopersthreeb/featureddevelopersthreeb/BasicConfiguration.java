b8906dcbf5870bdb9d8dcde3b8b22d646f705f31
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-02-03 23:32:31 -0600

    added swagger security cofiguration settings

--- src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/BasicConfiguration.java
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/BasicConfiguration.java
@@ -6,18 +6,24 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 public class BasicConfiguration extends WebSecurityConfigurerAdapter {
-
+     
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/").permitAll()
                                 .antMatchers(HttpMethod.GET, "/developers/random").permitAll()
 				.antMatchers(HttpMethod.GET, "/developers/randomshow").permitAll()
-                                .antMatchers(HttpMethod.GET, "/developers/getall")      
+                                .antMatchers(HttpMethod.GET, "/developers/getall").permitAll()
+                                .antMatchers("/**").permitAll()
+                                .mvcMatchers("swagger-ui.html")
+ 
                                 .permitAll().anyRequest().authenticated();
 	}
-       
+
 }
