c4889ce56d89988061eb8ae85304c99a1445e3b9
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-02-06 01:31:38 -0600

    added OpenApi configuration settings

---
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/OpenApiConfig.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nwmsufeatureddevelopersthreeb.featureddevelopersthreeb;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+ 
+@Configuration
+public class OpenApiConfig {
+ 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Featured Developer").description(
+                        "This App mainly focus to provide information about the developers in the GDP class."));
+    }
+}
