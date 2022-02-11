02a8df0f3e6a5a1dcc69c17c179f322f3b159df9
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-02-11 00:12:37 -0600

    added authentication controller class

---
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/AuthenticationController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nwmsufeatureddevelopersthreeb.featureddevelopersthreeb;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author S541906
+ */
+@Controller
+public class AuthenticationController {
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "auth";
+    }
+}
