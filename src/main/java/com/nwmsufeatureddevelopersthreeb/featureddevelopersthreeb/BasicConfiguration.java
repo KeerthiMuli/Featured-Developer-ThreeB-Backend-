bfb73a8ab66b4e6ec60ce68945294d3811631f3f
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-02-11 10:36:47 -0600

    added registration details

--- src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/BasicConfiguration.java
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/BasicConfiguration.java
@@ -1,11 +1,17 @@
 package com.nwmsufeatureddevelopersthreeb.featureddevelopersthreeb;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
@@ -13,9 +19,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebSecurity
 @EnableWebMvc
 public class BasicConfiguration extends WebSecurityConfigurerAdapter {
-     
         private final LogOut logoutHandler;
-
         public BasicConfiguration(LogOut logoutHandler) {
         this.logoutHandler = logoutHandler;
     }
@@ -32,5 +36,21 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
                                 .mvcMatchers("/logout")          
                                 .permitAll().anyRequest().authenticated().and().oauth2Login().and().logout();
 	}
-
+         @Bean
+    ReactiveClientRegistrationRepository getRegistration(
+            @Value("${spring.security.oauth2.client.provider.my-platform.token-uri}") String tokenUri,
+            @Value("${spring.security.oauth2.client.registration.my-platform.client-id}") String clientId,
+            @Value("${spring.security.oauth2.client.registration.my-platform.client-secret}") String clientSecret,
+            @Value("${spring.security.oauth2.client.registration.my-platform.scopes}") String scope
+    ) {
+        ClientRegistration registration = ClientRegistration
+                .withRegistrationId("my-platform")
+                .tokenUri(tokenUri)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .scope(scope)
+                .build();
+        return new InMemoryReactiveClientRegistrationRepository(registration);
+    }
 }
