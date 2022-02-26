1d5ab298adfd907d022c3649087dc8d2706c089f
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-02-25 18:57:10 -0600

    added featured developer  test cases class

---
+++ src/test/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/FeatureddevelopersthreebTests.java
@@ -0,0 +1,20 @@
+package com.nwmsufeatureddevelopersthreeb.featureddevelopersthreeb;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+      DevelopersControllerTests.class,
+       //RandomTestClass.class
+})
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class FeatureddevelopersthreebTests {
+
+@Test
+public void contextLoads() {
+	}	
+}
