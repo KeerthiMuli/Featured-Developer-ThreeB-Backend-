2893f4ad7aebf2e845c4e96a7db8cb1e94f31d29
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-02-25 18:46:05 -0600

    added constructors

--- src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/Developers.java
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/Developers.java
@@ -41,6 +41,7 @@ public class Developers {
   @Column(name = "image")
   private String Image;
 
+
   public String getDeveloperId() {
     return developerId;
   }
@@ -56,6 +57,22 @@ public class Developers {
   public void setFullName(String fullName) {
     this.fullName = fullName;
   }
+
+    public Developers() {
+    }
+  
+  
+
+    public Developers(String developerId, String fullName, String Emailaddress, String Goals, String Skills, String Linkedinurl, String Githuburl, String Image) {
+        this.developerId = developerId;
+        this.fullName = fullName;
+        this.Emailaddress = Emailaddress;
+        this.Goals = Goals;
+        this.Skills = Skills;
+        this.Linkedinurl = Linkedinurl;
+        this.Githuburl = Githuburl;
+        this.Image = Image;
+    }
 
   public String getEmailaddress() {
     return Emailaddress;
