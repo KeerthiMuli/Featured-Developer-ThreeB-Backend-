b7b6b21be417237818f8dcf78804777a5fbe782a
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-02-28 19:31:37 -0600

    added operation for swagger api

--- src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/DevelopersController.java
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/DevelopersController.java
@@ -47,7 +47,10 @@ public class DevelopersController {
   public List<Developers> getall() {
     return service.listAll();
   }
- 
+  
+  @Operation(
+      summary = "To add developer details",
+      description = "This will add list of all developers details")
   // To create new developers
 
 	@PostMapping("/developers/create")
