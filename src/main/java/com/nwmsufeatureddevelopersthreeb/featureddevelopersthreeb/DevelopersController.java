800b9e9ca9eef56525530f2c79999ff112e83fea
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-03-03 18:21:12 -0600

    added update method

--- src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/DevelopersController.java
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/DevelopersController.java
@@ -7,7 +7,10 @@ package com.nwmsufeatureddevelopersthreeb.featureddevelopersthreeb;
 
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
@@ -66,4 +69,45 @@ public class DevelopersController {
   public void delete(@PathVariable String developerId) {
     service.delete(developerId);
   }
+  
+  
+   @Operation(
+      summary = "To update developer details",
+      description = "This will update list of all developers details")
+  // To update developers details
+
+	@PutMapping("/developers/update/{developerId}")
+	public ResponseEntity<Developers> update(@RequestBody Developers developers, @PathVariable String developerId) {
+		try {
+			Developers existDevelopers = service.get(developerId);
+		    if(developers.getDeveloperId() != null)
+			existDevelopers.setDeveloperId(developers.getDeveloperId());
+		    service.save(existDevelopers);
+		    if(developers.getEmailaddress() != null)
+			existDevelopers.setEmailaddress(developers.getEmailaddress());
+		    service.save(existDevelopers);
+		    if(developers.getFullName() != null)
+			existDevelopers.setFullName(developers.getFullName());
+		    service.save(existDevelopers);
+		    if(developers.getGithuburl() != null)
+			existDevelopers.setGithuburl(developers.getGithuburl());
+		    service.save(existDevelopers);
+		    if(developers.getGoals() != null)
+			existDevelopers.setGoals(developers.getGoals());
+		    service.save(existDevelopers);
+		    if(developers.getImage() != null)
+			existDevelopers.setImage(developers.getImage());
+		    service.save(existDevelopers);
+		    if(developers.getLinkedinurl() != null)
+			existDevelopers.setLinkedinurl(developers.getLinkedinurl());
+		    service.save(existDevelopers);
+		    if(developers.getSkills() != null)
+			existDevelopers.setSkills(developers.getSkills());
+		    service.save(existDevelopers);		
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (NoSuchElementException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
 }
