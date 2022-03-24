3b56d8c2e841f7748330963412b791a8c37453ea
Keerthi Muli <keerthi.11rdy@gmail.com>
2022-03-24 13:00:46 -0500

    added user service class

---
+++ src/main/java/com/nwmsufeatureddevelopersthreeb/featureddevelopersthreeb/UserService.java
@@ -0,0 +1,18 @@
+package com.nwmsufeatureddevelopersthreeb.featureddevelopersthreeb;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	 @Autowired
+	UserRepository userRepository;
+	public List<User>listAll(){
+		return userRepository.findAll();
+	}
+	
+   public User save(User user) {
+	   return this.userRepository.save(user);
+   }
+
+}
